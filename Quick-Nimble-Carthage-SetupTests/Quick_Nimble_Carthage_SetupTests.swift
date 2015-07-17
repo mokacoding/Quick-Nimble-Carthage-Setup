2d4511738d4d331be76be1c981089262800903fe
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-07-17 16:58:36 +1000

    Add sample spec

--- Quick-Nimble-Carthage-SetupTests/Quick_Nimble_Carthage_SetupTests.swift
+++ Quick-Nimble-Carthage-SetupTests/Quick_Nimble_Carthage_SetupTests.swift
@@ -1,36 +1,16 @@
-//
-//  Quick_Nimble_Carthage_SetupTests.swift
-//  Quick-Nimble-Carthage-SetupTests
-//
-//  Created by Giovanni on 17/07/2015.
-//  Copyright © 2015 mokacoding. All rights reserved.
-//
+import Quick
+import Nimble
 
-import XCTest
-@testable import Quick_Nimble_Carthage_Setup
+class QuickNimbleCarthageSpec: QuickSpec {
+  override func spec() {
+    describe("Setting up Quick and Nimble for testing using Carthage") {
+      it("is not very hard") {
+        expect(true).to(beTruthy())
+      }
 
-class Quick_Nimble_Carthage_SetupTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      it("works very well") {
+        expect(20 * 2 + 3 - 1).to(equal(42))
+      }
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
 }
