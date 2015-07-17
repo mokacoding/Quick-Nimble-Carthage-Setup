850bebfed2adccf403ee584915e4afaa389af6db
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-07-17 16:34:03 +1000

    Initial Commit

---
+++ Quick-Nimble-Carthage-SetupTests/Quick_Nimble_Carthage_SetupTests.swift
@@ -0,0 +1,36 @@
+//
+//  Quick_Nimble_Carthage_SetupTests.swift
+//  Quick-Nimble-Carthage-SetupTests
+//
+//  Created by Giovanni on 17/07/2015.
+//  Copyright © 2015 mokacoding. All rights reserved.
+//
+
+import XCTest
+@testable import Quick_Nimble_Carthage_Setup
+
+class Quick_Nimble_Carthage_SetupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
